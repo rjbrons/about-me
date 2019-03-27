4e4edd2a0ced8dbc42adf0002cb01ce9cc1a466a
Rob Bronson <robert.j.bronson@gmail.com>
2019-03-27 16:17:28 -0700

    added js to get and use username

--- js/app.js
+++ js/app.js
@@ -9,9 +9,19 @@ let questions = [
 ];
 
 let answerChoices = ['yes', 'no', 'y', 'n'];
+let correctAnswers = [
+  ['no', 'n'],
+  ['yes', 'y'],
+  ['yes', 'y'],
+  ['no', 'n'],
+  ['yes', 'y']
+];
+let userName = prompt("Hello.  Let's play a trivia game.  What is your name?");
 let userAnswers = [];
+let correctGuesses = 0;
 
 for (let x = 0; x < questions.length; x++) {
+  let validationStatement = '';
   while (!userAnswers[x]) {
     userAnswers[x] = prompt(questions[x]);
     console.log('User input was: ' + userAnswers[x]);
@@ -20,7 +30,25 @@ for (let x = 0; x < questions.length; x++) {
       userAnswers[x] = null;
     }
   }
+
+  if (correctAnswers[x].includes(userAnswers[x])) {
+    validationStatement = 'correct!';
+    correctGuesses++;
+  } else {
+    validationStatement = 'incorrect.';
+  }
+  alert('You answered ' + userAnswers[x] + '.  That is ' + validationStatement);
 }
+
+alert(
+  'Well ' +
+    userName +
+    '.  Lets see how you did.  You have guessed ' +
+    correctGuesses +
+    ' out of ' +
+    questions.length +
+    ' answers correctly.'
+);
 
 console.log('Final user answers to questions - ');
 for (let x = 0; x < questions.length; x++) {
