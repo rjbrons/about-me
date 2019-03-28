cb33953324f9ea0ed62ef66deab3684be9b9e8d3
Rob Bronson <robert.j.bronson@gmail.com>
2019-03-27 20:43:46 -0700

    added number guess quetion and dried out code

--- js/app.js
+++ js/app.js
@@ -7,24 +7,26 @@ let questions = [
   'Have I ever been divorced?',
   'Do I like avocados?'
 ];
-
-let countries = [
-  'Ireland',
-  'Spain',
-  'Mexico',
-  'Canada',
-  'Afghanistan',
-  'Iraq',
-  'Kuwait',
-  'Kyrgyzstan'
-];
+let totalQuestions = 2 + questions.length;
 let answerChoices = ['yes', 'no', 'y', 'n'];
+
 let correctAnswers = [
   ['no', 'n'],
   ['yes', 'y'],
   ['yes', 'y'],
   ['no', 'n'],
   ['no', 'n']
+];
+
+let countries = [
+  'ireland',
+  'spain',
+  'mexico',
+  'canada',
+  'afghanistan',
+  'iraq',
+  'kuwait',
+  'kyrgyzstan'
 ];
 let userName = prompt(`Hello.  Let's play a trivia game.  What is your name?`);
 let userAnswers = [];
@@ -52,24 +54,23 @@ for (let x = 0; x < questions.length; x++) {
 }
 
 //Countries I have been in guessing game.
-let countryTrue;
 let countryTries = 0;
-while (!countryTrue && countryTries < 4) {
+while (countryTries < 4) {
   countryGuess = prompt(
     'I have been in several other countries, can you guess one? (You have 4 tries to answer the question.)'
   );
-  if (countries.includes(countryGuess)) {
-    countryTrue = true;
+  if (countries.includes(countryGuess.toLowerCase())) {
     alert(
       'Congratulations ' + userName + ' I have been to ' + countryGuess + '.'
     );
     correctGuesses++;
+    break;
   } else {
     countryTries++;
     alert(
       'Sorry, ' +
         userName +
-        'I have not been to ' +
+        ' I have not been to ' +
         countryGuess +
         '.  You have ' +
         (4 - countryTries) +
@@ -78,13 +79,46 @@ while (!countryTrue && countryTries < 4) {
   }
 }
 
+//Guess a number
+let highLowCorrection;
+let numberGuessCount = 0;
+let myNumber = Math.floor(Math.random() * 20) + 1;
+console.log(myNumber);
+alert(
+  'I have rolled a 20 sided die.  You must guess the number.  You have 6 tries.'
+);
+while (numberGuessCount < 6) {
+  var userNumberGuess = parseInt(prompt('What is your guess?'));
+  numberGuessCount++;
+  console.log('Guess count: ' + numberGuessCount);
+  console.log('User number guessed: ' + userNumberGuess);
+  if (userNumberGuess === myNumber) {
+    alert('You did it!! That was the number I rolled.');
+    correctGuesses++;
+    break;
+  } else if (userNumberGuess < myNumber) {
+    highLowCorrection = 'low';
+  } else if (userNumberGuess > myNumber) {
+    highLowCorrection = 'high';
+  }
+  alert(
+    'Sorry, ' +
+      userName +
+      ' that guess was too ' +
+      highLowCorrection +
+      ' try again.  You have ' +
+      (6 - numberGuessCount) +
+      ' tries left.'
+  );
+}
+
 alert(
   'Well ' +
     userName +
     '.  Lets see how you did.  You have guessed ' +
     correctGuesses +
     ' out of ' +
-    questions.length +
+    totalQuestions +
     ' answers correctly.'
 );
 
